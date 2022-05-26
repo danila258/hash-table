e7c8db257a4ca244afdbe2dc9ba09aa37839b9dc
danila <danilasidorov258@gmail.com>
2022-05-26 23:58:23 +0300

    add loadFactor, add collisionsFactor

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,8 +1,20 @@
 #include "HashTable.h"
 
-HashTable::HashTable(int length)
-{
-    this->_opacity = length;
+HashTable::HashTable(int length, double loadFactorCoef, double collisionsFactorCoef) : _opacity(length), _addCount(0),
+                     _loadFactorCoef(loadFactorCoef), _collisionsFactorCoef(collisionsFactorCoef), _collisions(0) {
+
+    if (_opacity < 0) {
+        _opacity = 0;
+    }
+
+    if (_loadFactorCoef < 0) {
+        _loadFactorCoef = 0.5;
+    }
+
+    if (_collisionsFactorCoef < 0) {
+        _collisionsFactorCoef = 0.5;
+    }
+
     _table = new std::list<int>[_opacity];
 }
 
@@ -15,14 +27,22 @@ void HashTable::insert(int key)
     int index = hashFunction(key);
 
     if ( !_table[index].empty() ) {
+        ++_collisions;
 
+        if (collisionsFactor() > _collisionsFactorCoef) {
+            //resize();
+        }
     }
 
     _table[index].push_back(key);
+    ++_addCount;
+
+    if (loadFactor() > _loadFactorCoef) {
+        //resize();
+    }
 }
 
-void HashTable::remove(int key)
-{
+void HashTable::remove(int key) {
     int index = hashFunction(key);
 
     std::list<int> :: iterator i;
@@ -35,7 +55,21 @@ void HashTable::remove(int key)
 
     if ( i != _table[index].end() ) {
         _table[index].erase(i);
+
+        --_addCount;
+
+        if ( !_table[index].empty() ) {
+            --_collisions;
+        }
     }
+}
+
+double HashTable::loadFactor() const {
+    return (double) _addCount / _opacity;
+}
+
+double HashTable::collisionsFactor() const {
+    return (double) _collisions / _addCount;
 }
 
 void HashTable::showTable() {
@@ -48,5 +82,10 @@ void HashTable::showTable() {
 
         std::cout << '\n';
     }
+
+    std::cout << "addCount = " << _addCount << '\n';
+    std::cout << "loadFactor = " << loadFactor() << '\n';
+    std::cout << "collisions = " << _collisions << '\n';
+    std::cout << "collisionsFactor = " << collisionsFactor() << '\n';
 }
 
--- HashTable.h
+++ HashTable.h
@@ -7,17 +7,23 @@
 
 class HashTable {
 public:
-    HashTable(int length);
+    HashTable(int length, double loadFactorCoef = 0.5, double collisionsFactorCoef = 0.5);
 
     int hashFunction(int x);
     void insert(int x);
     void remove(int key);
+    double loadFactor() const;
+    double collisionsFactor() const;
     void showTable();
 
 private:
     std::list<int> *_table;
+
     int _opacity;
+    int _addCount;
     int _collisions;
+    double _loadFactorCoef;
+    double _collisionsFactorCoef;
 };
 
 
